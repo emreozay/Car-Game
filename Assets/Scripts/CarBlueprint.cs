02131db66e7b779c64875343c98a6b18021dc97d
Emre Özay <emreozay@mail.com>
2022-01-14 21:51:30 +0300

    Car unlock system added

---
+++ Assets/Scripts/CarBlueprint.cs
@@ -0,0 +1,8 @@
+[System.Serializable]
+public class CarBlueprint
+{
+    public string name;
+    public int index;
+    public int price;
+    public bool isUnlocked;
+}
--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -1,14 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ShopManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] carModels;
     [SerializeField] private int currentCarIndex;
+    [SerializeField] private Button buyButton;
+    [SerializeField] private CarBlueprint[] cars;
+    [SerializeField] private TextMeshProUGUI moneyText;
+
 
     void Start()
     {
+        UpdateMoneyText();
+
+        foreach (CarBlueprint car in cars)
+        {
+            if (car.price == 0)
+                car.isUnlocked = true;
+            else
+                car.isUnlocked = PlayerPrefs.GetInt(car.name, 0) == 0 ? false : true;
+        }
+
         currentCarIndex = PlayerPrefs.GetInt("SelectedCar", 0);
 
         foreach (GameObject car in carModels)
@@ -20,7 +36,12 @@ public class ShopManager : MonoBehaviour
 
     void Update()
     {
-        
+        UpdateUI();
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            PlayerPrefs.SetInt("Money", PlayerPrefs.GetInt("Money", 0) + 20);
+            UpdateMoneyText();
+        }
     }
 
     public void ChangeNextCar()
@@ -32,6 +53,11 @@ public class ShopManager : MonoBehaviour
             currentCarIndex = 0;
 
         carModels[currentCarIndex].SetActive(true);
+
+        CarBlueprint c = cars[currentCarIndex];
+        if (!c.isUnlocked)
+            return;
+
         PlayerPrefs.SetInt("SelectedCar", currentCarIndex);
     }
 
@@ -44,6 +70,53 @@ public class ShopManager : MonoBehaviour
             currentCarIndex = carModels.Length - 1;
 
         carModels[currentCarIndex].SetActive(true);
+
+        CarBlueprint c = cars[currentCarIndex];
+        if (!c.isUnlocked)
+            return;
+
         PlayerPrefs.SetInt("SelectedCar", currentCarIndex);
+    }
+
+    public void UnlockCar()
+    {
+        CarBlueprint c = cars[currentCarIndex];
+
+        PlayerPrefs.SetInt(c.name, 1);
+        PlayerPrefs.SetInt("SelectedCar", currentCarIndex);
+
+        c.isUnlocked = true;
+
+        PlayerPrefs.SetInt("Money", PlayerPrefs.GetInt("Money", 0) - c.price);
+        UpdateMoneyText();
+    }
+
+    private void UpdateMoneyText()
+    {
+        moneyText.text = "$" + PlayerPrefs.GetInt("Money", 0);
+    }
+
+    private void UpdateUI()
+    {
+        CarBlueprint c = cars[currentCarIndex];
+
+        if (c.isUnlocked)
+        {
+            buyButton.gameObject.SetActive(false);
+        }
+        else
+        {
+            buyButton.gameObject.SetActive(true);
+            buyButton.GetComponentInChildren<TextMeshProUGUI>().text = "Buy - $" + c.price;
+
+            if (c.price <= PlayerPrefs.GetInt("Money", 0))
+            {
+                buyButton.interactable = true;
+            }
+            else
+            {
+                buyButton.interactable = false;
+            }
+        }
     }
 }
